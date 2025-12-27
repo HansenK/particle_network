629825ce6b7326bf1c374d0ecd69b4e4bfdebae7
Thiago Klein <thiagohansenklein@gmail.com>
2025-12-27 17:30:14 -0300

    Add comments to utils functions

--- utils.js
+++ utils.js
@@ -2,14 +2,38 @@ import Particle from "./particle.js";
 
 const canvas = document.getElementById("canvas");
 
-export const getRandomNumber = (min = 0, max = 1, round = false) => {
-  if (max <= min) {
-    throw new Error("Make sure max value is greater than min values");
+/**
+ * Generates a random integer within a specified inclusive range.
+ * Both min and max values are inclusive.
+ * Throws an error if max is less than min.
+ *
+ * @param {number} [min=0] - The minimum integer value (inclusive).
+ * @param {number} [max=1] - The maximum integer value (inclusive).
+ * @returns {number} A random integer between min and max (inclusive).
+ * @throws {Error} If max is less than min.
+ */
+export const getRandomNumber = (min = 0, max = 1) => {
+  if (max < min) {
+    throw new Error("Make sure max value is greater than min value");
   }
-  const randomNumber = Math.random() * (max - min) + min;
-  return round ? Math.round(randomNumber) : randomNumber;
+
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 };
 
+/**
+ * Calculates the Euclidean distance between two points in 2D space.
+ * Uses the Pythagorean theorem to compute the straight-line distance.
+ *
+ * @param {Object} pointA - The first point with x and y coordinates
+ * @param {number} pointA.posX - X coordinate of the first point
+ * @param {number} pointA.posY - Y coordinate of the first point
+ * @param {Object} pointB - The second point with x and y coordinates
+ * @param {number} pointB.posX - X coordinate of the second point
+ * @param {number} pointB.posY - Y coordinate of the second point
+ * @returns {number} The distance between the two points
+ */
 export const getDistanceBetween = (
   { posX: posAX, posY: posAY },
   { posX: posBX, posY: posBY }
@@ -19,6 +43,14 @@ export const getDistanceBetween = (
   );
 };
 
+/**
+ * Generates an array of Particle objects with random positions and sizes.
+ * Each particle is placed at a random position within the canvas bounds
+ * and assigned a random radius between 1 and 4 pixels.
+ *
+ * @param {number} numberOfParticles - The number of particles to generate
+ * @returns {Particle[]} An array of Particle instances
+ */
 export const generateParticles = (numberOfParticles) => {
   const particles = [];
   for (let i = 0; i <= numberOfParticles; i++) {
