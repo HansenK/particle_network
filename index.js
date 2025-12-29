3d8ac9ab6ab36e10b946dfeb66644952c7587e80
Thiago Klein <thiagohansenklein@gmail.com>
2025-12-29 18:37:41 -0300

    Refactor index.js to export ParticleNetwork directly and remove canvas initialization code.

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 import ParticleNetwork from "./particle-network.js";
-
-const canvas = document.getElementById("canvas");
-const particleNetwork = new ParticleNetwork(canvas);
-particleNetwork.start();
-
-export default particleNetwork;
+export default ParticleNetwork;
